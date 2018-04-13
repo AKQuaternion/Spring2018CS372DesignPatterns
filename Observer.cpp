3a3f32249cf1bb6c1cfeaee2a853e8c95545cf0d
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-13 15:44:04 -0800

    Create Observer files

---
+++ Observer.cpp
@@ -0,0 +1,8 @@
+//
+//  Observer.cpp
+//  DesignPatterns
+//
+//  Created by Chris Hartman on 4/13/18.
+//
+
+#include "Observer.hpp"
---
+++ Observer.hpp
@@ -0,0 +1,13 @@
+//
+//  Observer.hpp
+//  DesignPatterns
+//
+//  Created by Chris Hartman on 4/13/18.
+//
+
+#ifndef Observer_hpp
+#define Observer_hpp
+
+#include <stdio.h>
+
+#endif /* Observer_hpp */
